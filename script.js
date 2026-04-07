9dfcbcd0cf1592751d17f22a74e32f6ae6cc085e
Eugene-Yu78 <yue9933@my.sduhsd.net>
2026-04-07 20:29:56 +0000

    play

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-// add javascript here
+// Game Start
+let answer = 0;
+let guessCount = 0;
+let totalWins = 0;
+let totalGuesses = 0;
+let scores = 0;
+
+//player Name
+let playerName = prompt("Enter your name:");
+//play
+document.getElementById("playBtn").addEventListener("click",function(){
+    let radios = document.getElementsByName("level");
+    let range = 3;
+    for(let i = 0; i < radios.length; i++){
+        if(radios[i].checked){
+            range=parseint(radios[i].value);
+
+        }
+    }
+})
+answer = Math.floor(Math.random() * range) + 1;
+
+document.getElementById("msg").textContent = playerName + ", guess a number between 1 and " + range;
