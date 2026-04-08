f79f5b303d0959cfcea7c83e26db11b8500da31e
Eugene-Yu78 <yue9933@my.sduhsd.net>
2026-04-08 20:17:45 +0000

    guessing and giveup

--- script.js
+++ script.js
@@ -28,7 +28,40 @@ document.getElementById("playBtn").addEventListener("click",function(){
     for (let i=0; i<levelRadios.length; i++){
         levelRadios[i].disabled = true;
     }
-    
+});
 
+//guess
+document.getElementById("guessBtn").addEventListener("click", function(){
+    let guessInput = document.getElementById("guess");
+    let guess = parseInt(guessInput.value);
+    guessCount++;
+    totalGuesses++;
+    if(guess === answer){
+        document.getElementById("msg").textContent = "Congratulations " + playerName + "! You guessed the number in " + guessCount + " guesses.";
+        totalWins++;
+        scores.push(guessCount);
+        endGame();
+    } else if (guess < answer){
+        document.getElementById("msg").textContent = "Too low! Try again.";
+    } else {
+        document.getElementById("msg").textContent = "Too high! Try again.";
+    }
+    guessInput.value = " ";
+});
 
-})
+//give up
+document.getElementById("giveUpBtn").addEventListener("click", function(){
+    document.getElementById("msg").textContent = "The correct number was " + answer + ". Better luck next time, " + playerName + "!";
+    endGame();
+});
+
+function endGame(){
+    document.getElementById("guessBtn").disabled = true;
+    document.getElementById("giveUpBtn").disabled = true;
+    document.getElementById("playBtn").disabled = false;
+    let levelRadios = document.getElementsByName("level");
+    for (let i=0; i<levelRadios.length; i++){
+        levelRadios[i].disabled = false;
+    }
+    guessCount = 0;
+}
